640860f94b377c6af619ce6a043c5e87695f0f75
Wagner Oliveira dos Santos <prog.wagner@gmail.com>
2019-05-10 02:32:39 -0300

    Backgrounds

---
+++ space_map/parallax-space-big-planet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="parallax-space-big-planet" tilewidth="32" tileheight="32" tilecount="4" columns="2">
+ <image source="parallax-space-big-planet.png" width="88" height="87"/>
+</tileset>
